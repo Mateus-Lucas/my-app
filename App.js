82811dfb70efbbd7bbab6e1d8a585c334a2a4d03
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-08-16 21:50:55 -0300

    Criando Projeto

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+
+const Card = ({ title, description, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.card} onPress={onPress}>
+      <View style={styles.cardContent}>
+        <Text style={styles.cardTitle}>{title}</Text>
+        <Text style={styles.cardDescription}>{description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+         source={{uri:'https://i.pinimg.com/originals/cd/6d/6b/cd6d6b01e938ef486734ce8dd2544ae5.png'}}
+         style={{width: 400, height: 400}}         
+      />
     </View>
   );
 }
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
