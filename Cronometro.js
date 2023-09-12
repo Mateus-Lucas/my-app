d20509518b2807fae0bbc801d6b198f6ab413321
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-09-12 18:59:20 -0300

    Cronometro Finalizado

--- Cronometro.js
+++ Cronometro.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function Cronometro() {
+  const [horas, setHoras] = useState(0);
   const [minutos, setMinutos] = useState(0);
   const [segundos, setSegundos] = useState(0);
-  const [milissegundos, setMilissegundos] = useState("00"); 
+  const [milissegundos, setMilissegundos] = useState("00");
   const [isRunning, setIsRunning] = useState(false);
+  const [ultimoTempo, setUltimoTempo] = useState(null);
 
   useEffect(() => {
     let customInterval;
@@ -14,23 +16,28 @@ export default function Cronometro() {
       customInterval = setInterval(() => {
         if (milissegundos === "99") {
           if (segundos === 59) {
-            setMinutos((prevMinutos) => prevMinutos + 1);
+            if (minutos === 59) {
+              setHoras((prevHoras) => prevHoras + 1);
+              setMinutos(0);
+            } else {
+              setMinutos((prevMinutos) => prevMinutos + 1);
+            }
             setSegundos(0);
           } else {
             setSegundos((prevSegundos) => prevSegundos + 1);
           }
-          setMilissegundos("00"); 
+          setMilissegundos("00");
         } else {
-          let newMilissegundos = (parseInt(milissegundos, 10) + 1).toString().padStart(2, '0'); 
+          let newMilissegundos = (parseInt(milissegundos, 10) + 1).toString().padStart(2, '0');
           setMilissegundos(newMilissegundos);
         }
-      }, 10); 
+      }, 10); // Atualização a cada 10 milissegundos
     } else {
       clearInterval(customInterval);
     }
 
     return () => clearInterval(customInterval);
-  }, [isRunning, milissegundos, segundos]);
+  }, [isRunning, milissegundos, segundos, minutos, horas]);
 
   const startTimer = () => {
     setIsRunning(true);
@@ -38,22 +45,37 @@ export default function Cronometro() {
 
   const stopTimer = () => {
     setIsRunning(false);
+    setUltimoTempo({ horas, minutos, segundos, milissegundos });
   };
 
   const clearTimer = () => {
     setIsRunning(false);
+    setHoras(0);
     setMinutos(0);
     setSegundos(0);
-    setMilissegundos("00"); 
+    setMilissegundos("00");
+    setUltimoTempo(null);
+  };
+
+  const formatTempo = (tempo) => {
+    if (!tempo) {
+      return '00:00:00.00';
+    }
+
+    const { horas, minutos, segundos, milissegundos } = tempo;
+    return `${horas < 10 ? '0' + horas : horas}:${minutos < 10 ? '0' + minutos : minutos}:${segundos < 10 ? '0' + segundos : segundos}.${milissegundos}`;
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.textCustom}>
-        {minutos < 10 ? '0' + minutos : minutos}:
-        {segundos < 10 ? '0' + segundos : segundos}:
-        {milissegundos}
+        {formatTempo({ horas, minutos, segundos, milissegundos })}
       </Text>
+      {ultimoTempo && (
+        <Text style={styles.ultimoTempo}>
+          Último tempo: {formatTempo(ultimoTempo)}
+        </Text>
+      )}
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={[styles.button, { backgroundColor: 'green' }]} onPress={startTimer} disabled={isRunning}>
           <Text style={styles.buttonText}>Iniciar</Text>
@@ -87,11 +109,15 @@ const styles = StyleSheet.create({
     marginRight: 25
   },
   buttonText: {
-    fontSize: 18,
+    fontSize: 30,
     color: 'white',
     textAlign: 'center',
   },
   textCustom: {
-    fontSize: 150,
+    fontSize: 90,
+  },
+  ultimoTempo: {
+    fontSize: 30,
+    marginTop: 10,
   },
 });
